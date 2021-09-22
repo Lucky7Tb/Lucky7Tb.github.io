bcf432e6f881496d2046a5243cebd8ef6850e467
Lucky7Tb <Luckytri101@gmail.com>
2021-09-23 00:17:49 +0700

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,6 @@ mix
 		mix.copy('src/index.html', 'dist');
 		mix.copyDirectory('src/img', 'dist/img');
 	})
-	.css('src/css/style.css', 'css')
 	.css('src/css/hover.css', 'css')
 	.js('src/js/app.js', 'js')
-	.postCss('src/css/style.css', 'css', [require('tailwindcss')]);
+	.postCss('src/css/app.css', 'css', [require('tailwindcss')]);
