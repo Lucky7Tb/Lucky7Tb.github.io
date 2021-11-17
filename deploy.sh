10db2e46d9a0e14f9e11bcce2bb702635adc17db
Lucky7Tb <Luckytri101@gmail.com>
2021-11-18 01:23:24 +0700

    update sh

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Lucky7Tb/Lucky7Tb.github.io.git main
+git push -f git@github.com:Lucky7Tb/Lucky7Tb.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
